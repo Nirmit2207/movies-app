031512fca3a8f40a40f2cd2258dfa81ed818b784
Nirmit <nirmitsinghal00@gmail.com>
2022-04-24 19:53:43 +0530

    added debouncing

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react'
+import { useDebounce } from 'use-debounce';
 
 import './App.css'
 import SearchIcon from './search.svg'
@@ -9,18 +10,19 @@ const API_URL = 'https://www.omdbapi.com?apikey=1fb22392'
 function App() {
 
   const [movies, setMovies] = useState([])
-  const [searchTerm, setSearchTerm] = useState('')
+  const [searchTerm, setSearchTerm] = useState('Jurassic Park')
+  const [debouncedValue] = useDebounce(searchTerm, 500)
 
   const searchMovies = async (title) => {
     const response = await fetch(`${API_URL}&s=${title}`)
     const data = await response.json()
-
-    setMovies(data.Search)
+    
+    data.Response !== 'False' && setMovies(data.Search)
   }
 
   useEffect(() => {
-    searchMovies('Batman')
-  }, [])
+    (debouncedValue !== '') && searchMovies(debouncedValue)
+  }, [debouncedValue])
 
   return (
     <div className="app">
@@ -32,15 +34,13 @@ function App() {
           value={searchTerm}
           onChange={(e) => setSearchTerm(e.target.value)}
         />
-        <img src={SearchIcon} alt="search"
-          onClick={() => searchMovies(searchTerm)}
-        />
+        <img src={SearchIcon} alt="search" />
       </div>
 
       {
         movies.length > 0 ?
           (<div className="container">
-            {movies.map(movie => <MovieCard movie={movie} />)}
+            {movies.map(movie => <MovieCard movie={movie} key={movie.imdbID} />)}
           </div>) : (<div className="empty">
             <h2>No movies found</h2>
           </div>)
